35fd80ebc2401c44fd14de866aa362c5cc261cc7
alexei503 <00126118@uca.edu.sv>
2019-09-25 10:59:49 -0600

    javas

--- Laboratorios/Laboratorio 5/eje1.js
+++ Laboratorios/Laboratorio 5/eje1.js
@@ -1,2 +1,22 @@
-j = typeof("h")
-console.log(j)
+
+
+function verificador(areglo){
+    var numero = 0;
+    var palabra = 0;
+    for(var i = 0; i<areglo.length;i++){
+        if(typeof(areglo[i]) == "string"){
+        palabra++;
+        }else {
+            numero++
+        }
+                }               
+                console.log(palabra);
+                console.log(numero);
+            }
+
+
+
+
+
+var areglo = [1,1,"hola","pan","arroz"]
+verificador(areglo);
